b47881dc5e0491a08f2e5852ab6eb4e3dc79eb75
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-05-02 10:56:29 +0700

    make instance of app

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { parseFlags } from "./lib/parser"
+import dotenv from "dotenv"
+import express from "express"
+import { env } from '@/lib/env';
+import { RoomApi } from "./app";
+
+const flags = parseFlags(process.argv)
+dotenv.config({
+    path: flags.env
+})
+
+const app = express()
+const roomapi = new RoomApi(app, {
+    port: flags.port ? Number(flags.port) : env.get('PORT').toNumber(9000)
+})
+
+roomapi.start()
+roomapi.shutdown()
