699c67cd41b09c98915f8fd3617036711aa1c840
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-05-02 12:59:36 +0700

    make user-room model

---
+++ src/app/module/user-room/user-room.model.ts
@@ -0,0 +1,34 @@
+import { DataTypes, Model } from "sequelize";
+import { Room } from "../room/room.model";
+import { User } from "../account/account.model";
+import { Database } from "@/db";
+
+const db = Database.getSQLInstance()
+
+export class UserRoom extends Model {}
+
+UserRoom.init({
+    id: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    userId: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    },
+    roomId: {
+        type: DataTypes.INTEGER.UNSIGNED,
+        references: {
+            model: Room,
+            key: 'id'
+        }
+    }
+}, {
+    sequelize: db,
+    modelName: 'UserRoom',
+    tableName: 'user_rooms'
+});
