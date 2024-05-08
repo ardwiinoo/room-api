8853fa9f89b9bdbbf2a8c078ebcd7ba38b56b950
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-05-08 20:50:06 +0700

    chnage type sqlConfig not static

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize"
 import mongoose from "mongoose"
-import { env } from "@/lib/env"
+import { env } from "../lib/env"
 
 export interface Connection {
     open(): Promise<void>
@@ -12,11 +12,12 @@ export class Database implements Connection {
     private static sqlInstance: Sequelize     
     private static mdbInstance: typeof mongoose
     
-    private static sqlConfig = {
+    private sqlConfig = {
         database: env.get('DB_NAME').toString(),
-        username: env.get('DB_NAME').toString(),
+        username: env.get('DB_USER').toString(''),
         password: env.get('DB_PASS').toString(),
         host: env.get('DB_HOST').toString(),
+        port: env.get('DB_PORT').toNumber()
     }
     private static mdbUri = env.get('MONGO_URI').toString()
     
@@ -31,21 +32,22 @@ export class Database implements Connection {
     async open(): Promise<void> {
         if (Database.sqlInstance) return
 
-        Database.sqlInstance = new Sequelize(Database.sqlConfig.database, Database.sqlConfig.username, Database.sqlConfig.password, {
-            host: Database.sqlConfig.host,
-            dialect: 'mysql'
+        Database.sqlInstance = new Sequelize(this.sqlConfig.database, this.sqlConfig.username, this.sqlConfig.password, {
+            host: this.sqlConfig.host,
+            dialect: 'mysql',
+            port: this.sqlConfig.port
         })
 
         await Database.sqlInstance.authenticate()
 
-        if (Database.mdbInstance) return
+        // if (Database.mdbInstance) return
 
-        await mongoose.connect(Database.mdbUri)
-        Database.mdbInstance = mongoose
+        // await mongoose.connect(Database.mdbUri)
+        // Database.mdbInstance = mongoose
     }
 
     async close(): Promise<void> {
-        if (Database.mdbInstance) await Database.mdbInstance.disconnect()
+        // if (Database.mdbInstance) await Database.mdbInstance.disconnect()
         if (Database.sqlInstance) await Database.sqlInstance.close()
     }
 }
