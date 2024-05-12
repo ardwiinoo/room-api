503e33f2c95de3de9d3a90dfad2eaf870b3d5a80
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-05-12 12:24:17 +0700

    use instance of RTC

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import express from "express"
 import { env } from './lib/env';
 import { RoomApi } from "./app";
+import { RTC } from "./plugin/rtc";
 
 const flags = parseFlags(process.argv)
 dotenv.config({
@@ -13,6 +14,9 @@ const app = express()
 const roomapi = new RoomApi(app, {
     port: flags.port ? Number(flags.port) : env.get('PORT').toNumber(9000)
 })
+
+// real time connection
+roomapi.use(new RTC())
 
 roomapi.start()
 roomapi.shutdown()
