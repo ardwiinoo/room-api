56189f1865cffda0a968d17bdba6eb29bfb5975e
Arif Dwi Nugroho <ardwiinoo@gmail.com>
2024-05-23 12:53:03 +0700

    user rtc instance and create handler socket

--- src/app/module/chat/chat.api.ts
+++ src/app/module/chat/chat.api.ts
@@ -1,16 +1,27 @@
 import { Service } from "../../index"
 import express, { Express } from "express"
-import { ChatStore } from "./chat.store"
+import { Server, Socket } from "socket.io"
+import { ChatEventHandler } from "./chat.event"
 
 export class ChatService implements Service {
     
-    private store = new ChatStore()
+    private rtc: Server
+    private eventHandler: ChatEventHandler
 
-    constructor(private app: Express) {}
+    constructor(private app: Express) {
+        this.rtc = this.app.rtc
+        this.eventHandler = new ChatEventHandler(this.rtc)
+    }
+
+    handlerSocket = (socket: Socket) => {
+        socket.on('chat', this.eventHandler.create(socket))
+        socket.on('chat-history', this.eventHandler.getHistories(socket))
+    }
     
     createRoutes(): void {
         const v1 = express.Router()
 
         this.app.use('/api/v1', v1)
+        this.rtc.on('connection', this.handlerSocket)
     }
 }
